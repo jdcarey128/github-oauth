4dfc8e6fb4e352df997197bb25e788c99de91c78
Joshua Carey <jdcarey128@gmail.com>
2021-01-01 16:28:54 -0700

    Add readme for app and code highlights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
-  def validate_user 
+  def validate_user
     redirect_to 'public/404.html' unless current_user
   end
 
@@ -11,6 +11,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
-
-
 end
